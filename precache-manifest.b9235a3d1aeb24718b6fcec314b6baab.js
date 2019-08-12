0427a2e8db3cf4c83dccb2043d1be4637ce09970
kirill-lalala <mr.ilikov@gmail.com>
2019-08-13 02:52:47 +0300

    Updates

---
+++ precache-manifest.b9235a3d1aeb24718b6fcec314b6baab.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2c473a5957c975ac324dc6a174babc3",
+    "url": "/kirill-lalala/.github.io/game/index.html"
+  },
+  {
+    "revision": "f05c6ecebd2597315041",
+    "url": "/kirill-lalala/.github.io/game/static/css/main.cb10e96c.chunk.css"
+  },
+  {
+    "revision": "7465540aca0a8605d3d6",
+    "url": "/kirill-lalala/.github.io/game/static/js/2.19004664.chunk.js"
+  },
+  {
+    "revision": "f05c6ecebd2597315041",
+    "url": "/kirill-lalala/.github.io/game/static/js/main.c95af904.chunk.js"
+  },
+  {
+    "revision": "4d665f3c4c551d02790b",
+    "url": "/kirill-lalala/.github.io/game/static/js/runtime~main.ccfa76fe.js"
+  },
+  {
+    "revision": "f03ead55c229c36d6dea1c3748d7e600",
+    "url": "/kirill-lalala/.github.io/game/static/media/heart.f03ead55.svg"
+  },
+  {
+    "revision": "684bae1bba78d8f151b4b6b862c37ffa",
+    "url": "/kirill-lalala/.github.io/game/static/media/selectArrow.684bae1b.svg"
+  },
+  {
+    "revision": "0006efe73a613ec64fd3e638bc545f23",
+    "url": "/kirill-lalala/.github.io/game/static/media/shield.0006efe7.svg"
+  },
+  {
+    "revision": "9002fed5c5975e959d4e10f2ff0f7a46",
+    "url": "/kirill-lalala/.github.io/game/static/media/sword.9002fed5.svg"
+  }
+]);
